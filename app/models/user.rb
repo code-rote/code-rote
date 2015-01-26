506edc8ccd883219bd84f1b1551a274806c7c75c
Angelo Cisneros <a.e.cisneros@gmail.com>
2015-01-26 10:48:20 -0800

    Modify Gemfile, use bcrypt for ActiveModels 'has_secure_password'. Add methods to User model #self.confirm, #password_required? #email_conf_required?; Add validations for :email, :email_confirmation, :password_confirmation. And add has_secure_password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,38 @@
 class User < ActiveRecord::Base
+  # use bcrypt and ActiveModel has_secure_password
+  has_secure_password
+
+
+  # validations
+  validates :email,
+            uniqueness: true,
+            confirmation: true
+  
+  validates :email_confirmation,
+            format: {with: /.+@+/},
+            presence: true,
+            if: :email_conf_required?
+
+  validates :password_confirmation,
+            presence: true,
+            length: {minimum: 8},
+            # password must not be al lowercase letters
+            format: {without: /\A[a-z]+\z/},
+            if: :password_required?
+
+  # associations
+
+  # class methods
+  def self.confirm(email_param, password_param)
+    user = User.find_by({email: email_param})
+    user.try(:authenticate, password_param)
+  end
+  
+  def password_required?
+    self.new_record? or !self.password.nil?
+  end
+
+  def email_conf_required?
+    self.new_record? or self.email_changed?
+  end
 end
