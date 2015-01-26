78aa732019a7b1cb2c3fa5034a71cdaf7dce0ce3
Angelo Cisneros <a.e.cisneros@gmail.com>
2015-01-25 20:43:29 -0800

    Add foundation 5 specific layouts for navigation, navigation links. Change the application.rb to include foundation. Change application.js to load foundation(); function on document.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,7 @@
 // listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require foundation
 //= require_tree .
 
 //= require prism.js
@@ -20,4 +21,8 @@
 // prism syntax highlighter script
 $(document).on('ready page:load', function() {
     Prism.highlightAll();
+});
+
+$(function() {
+  $(document).foundation();
 });
