b2d476a559a98ed57993be58ab2dc6fe810c76f4
Angelo Cisneros <a.e.cisneros@gmail.com>
2015-01-26 10:42:45 -0800

    Modify SessionsController with #new, #create, #destroy. Modify SessionsHelper with .current_user, .login, .logged_in? and .logout. Add SessionsHelper to application_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,28 @@ class SessionsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    user_params = params.require(:user)
+    user = User.confirm(*login_params)
+    if user
+      login(user)
+      redirect_to user_path(user)
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to login_path
+  end
+
+
+    private
+      def login_params
+        user = params.require(:user)
+        [user[:email], user[:password]]
+      end
+
 end
