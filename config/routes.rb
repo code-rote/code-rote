ce8a5108493f0e738eb986286071e90a19918bf2
Angelo Cisneros <a.e.cisneros@gmail.com>
2015-01-26 10:43:42 -0800

    Add session routes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root to: 'session#new'
+
+  # session routes
+  get '/signup', to: 'users#new', as: 'signup'
+  resources :sessions, only: [:create, :destroy]
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
+  delete '/sessions', to: 'sessons#destroy', as: 'logout'
 end
