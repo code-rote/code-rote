c80954fa9653603fe035588a07e674974e50a7f6
Emannuel Nevarez <nevarez.emannuel0@gmail.com>
2015-01-28 18:43:51 -0800

    User flow changes. Focused on Login.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,16 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+    #render "users/new"
   end
 
   def create
     user = User.new(user_params)
     if user.save
-      login(user)
-      redirect_to profile_path
+      #login(user)
+    #  redirect_to profile_path
+      redirect_to login_path
+
     else
       redirect_to signup_path
     end
